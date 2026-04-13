1fe4b0757b450dbb8600f4483b512fcd3cb3d800
bazelisek <windynour@gmail.com>
2026-04-13 14:45:53 +0200

    chemie13.4.

--- src/components/notes/subjects/chemistry/topics/index.ts
+++ src/components/notes/subjects/chemistry/topics/index.ts
@@ -1,5 +1,13 @@
 import { Topic } from "../..";
+import AlkenyAlkyny from "./organika/AlkenyAlkyny";
 
-const topics: Topic[] = [];
+const topics: Topic[] = [
+    {
+        id: "alkeny-alkyny",
+        name: 'Alkeny, Alkyny',
+        description: "Alkeny a alkyny jsou nasycené uhlovodíky, které obsahují dvojnou nebo trojnou vazbu mezi atomy uhlíku. Alkeny mají obecný vzorec CnH2n, zatímco alkyny mají obecný vzorec CnH2n-2. Tyto sloučeniny jsou důležité v organické chemii a mají různé využití v průmyslu a každodenním životě.",
+        homepage: AlkenyAlkyny
+    }
+];
 
 export default topics;
---
+++ src/components/notes/subjects/chemistry/topics/organika/AlkenyAlkyny/index.tsx
@@ -0,0 +1,17 @@
+export default function AlkenyAlkyny() {
+  return (
+    <div>
+      <object
+        data="/materials/Alkeny_alkyny1.pdf"
+        type="application/pdf"
+        width="100%"
+        height="100%"
+        style={{ minHeight: "900px" }}
+      >
+        <p>
+          <a href="/materials/Alkeny_alkyny1.pdf">Open PDF</a>
+        </p>
+      </object>
+    </div>
+  );
+}
