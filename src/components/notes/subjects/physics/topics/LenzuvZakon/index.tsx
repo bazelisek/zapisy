f5956d6c9d46ceb3482955de1c06a51e42be9fd6
bazelisek <windynour@gmail.com>
2026-04-13 18:42:32 +0200

    Fyzika13.4.

---
+++ src/components/notes/subjects/physics/topics/LenzuvZakon/index.tsx
@@ -0,0 +1,17 @@
+import Typography from "@/components/elements/Typography";
+import { Separator } from "@heroui/react";
+var Latex = require('react-latex');
+
+export default function LenzuvZakon() {
+    return (
+        <div>
+            <Typography>Zákon říká, že indukovaný proud bude vždy směřovat tak, aby vytvářel magnetické pole, které se snaží zabránit změně, která ho vyvolala.</Typography>
+            <Separator />
+            <Typography>Proud v obvodu má takový směr, že svým magnetickým polem působí proti změně magnetického indukčního toku, která je jeho příčinou.</Typography>
+            <Typography variant="note">Zkráceně, proud působí proti změně, která ho vyvolala</Typography>
+            <Typography>Také se nazývají jako <strong>Foucaltovy proudy</strong></Typography>
+            <Separator></Separator>
+            <Latex>$\phi = B \cdot S \cdot \cos(\alpha)$</Latex>
+        </div>
+    )
+}
--- src/components/notes/subjects/physics/topics/index.ts
+++ src/components/notes/subjects/physics/topics/index.ts
@@ -1,5 +1,14 @@
 import { Topic } from "../..";
+import LenzuvZakon from "./LenzuvZakon";
 
-const topics: Topic[] = [];
+const topics: Topic[] = [
+  {
+    id: "lenzuv-zakon",
+    name: "Lenzův zákon",
+    description:
+      "Lenzův zákon je fyzikální zákon, který popisuje směr indukovaného proudu v elektrickém obvodu.",
+    homepage: LenzuvZakon,
+  },
+];
 
 export default topics;
