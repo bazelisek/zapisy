8e0f59d37fe75226b54a47415eeeba499eda28f8
bazelisek <windynour@gmail.com>
2026-05-07 08:34:31 +0200

    Dejepis7.5.

--- src/components/notes/subjects/history/topics/WW2/PredWW2.tsx
+++ src/components/notes/subjects/history/topics/WW2/PredWW2.tsx
@@ -75,19 +75,66 @@ export default function PredWW2() {
           1936 - remilitarizace porýní (faktické zrušení Locarna)
         </Typography>
         <Typography>1936 - Spolupráce s Itálií a Japonskem</Typography>
-        <Typography level="h3">
-          Antisemitická politika
+        <Typography level="h3">Antisemitická politika</Typography>
+        <Typography>
+          Vyloučení židů ze státní správy, univerzit, lékařských a právnických
+          komor. To mělo za cíl podporu emigrace. Odcházeli především mladší,
+          vzdělanější a bohatší.
         </Typography>
-        <Typography>Vyloučení židů ze státní správy, univerzit, lékařských a právnických komor. To mělo za cíl podporu emigrace. Odcházeli především mladší, vzdělanější a bohatší.</Typography>
-        <Typography>Do roku 1939 jejich počet v Německu poklesl asi na polovinu. Odešli především ti bohatší, mladší, vzdělanější.</Typography>
-        <Typography>K dalším nátlakovým prostředkům patřil například bojkot židovských obchodů.</Typography>
+        <Typography>
+          Do roku 1939 jejich počet v Německu poklesl asi na polovinu. Odešli
+          především ti bohatší, mladší, vzdělanější.
+        </Typography>
+        <Typography>
+          K dalším nátlakovým prostředkům patřil například bojkot židovských
+          obchodů.
+        </Typography>
         <Typography level="h4">Norimberské rasové zákony: září 1935</Typography>
-        <Typography>Židé byli zbaveni  občanství, zákaz smíšených manželství</Typography>
-        <Typography>Křišťálová noc - 1938 - Největší německý pogrom, který přinesl 91 mrtvých, 7000 vypálených obchodů a synagog, 30k židů odvlečeno do koncentračních  táborů</Typography>
+        <Typography>
+          Židé byli zbaveni občanství, zákaz smíšených manželství
+        </Typography>
+        <Typography>
+          Křišťálová noc - 1938 - Největší německý pogrom, který přinesl 91
+          mrtvých, 7000 vypálených obchodů a synagog, 30k židů odvlečeno do
+          koncentračních táborů
+        </Typography>
         <Typography>Arizace - vyvlastňování židovského majetku</Typography>
         <Typography level="h4">Kultura</Typography>
-        <Typography>Odpor k modernímu umění, které bylo označováno jako zvrlé nebo tzv kulturní bolševismus. Preferován byl romantismus a realismus 19. století.</Typography>
-        <Typography>K propagaci režimu velmi dobře posloužily olympijské hry v Berlíně 1936</Typography>
+        <Typography>
+          Odpor k modernímu umění, které bylo označováno jako zvrlé nebo tzv
+          kulturní bolševismus. Preferován byl romantismus a realismus 19.
+          století.
+        </Typography>
+        <Typography>
+          K propagaci režimu velmi dobře posloužily olympijské hry v Berlíně
+          1936
+        </Typography>
+        <DateMark>7.5.2026</DateMark>
+        <Typography level="h4">Velká hospodářská krize v ČS</Typography>
+        <Typography>
+          V našich zemích vypukla se zpožděním a rok 29 byl rokem vrcholného
+          růstu. Ještě v letech 30 a první polovině 31 měla jen slabý dopad
+          (Zemědělství, potravinářský průmysl, spotřební průmysl), ale od léta
+          31 se rozjela naplno. Přišel hluboký propad ekonomiky, který se
+          zastavil až roku 33, kdy byla průmyslová výroba na 60% předkrizového
+          stavu a zahraniční obchod poklesl o 70%. 750k nezaměstananých.
+        </Typography>
+        <Typography>
+          Důsledky se projevily v podobě demonstrací, které někdy přerostly v
+          střety s policií,po kterých zůstávali někdy i mrtví. Nejvíce utrpěla
+          odvětví závislá na vývozu, tedy např. textil, sklářství, výroba
+          porcelánu apod. Ta se koncnetrovala především v oblastech obývaných
+          Němci.
+        </Typography>
+        <Typography>
+          V ekonomické oblasti došlo k výraznému oživení až roku 36 (ale ani 37
+          jsme ještě nedosáhli HDP předkrizového stavu), čemuž poměrně pomohly
+          masivní státní investice do obrany státu.
+        </Typography>
+        <Typography>
+          Negativním důsledkem byl nárůst nacionálního napětí a posilování
+          politických extermistů (fašisti, komunisti).
+        </Typography>
       </NoteAccordion>
     </>
   );
