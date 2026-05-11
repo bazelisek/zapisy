b5adba330af9306d33e3de4ecadd4b41bc9447d2
bazelisek <windynour@gmail.com>
2026-05-11 11:52:18 +0200

    Fyzika11.5.

--- src/components/notes/subjects/physics/topics/StridavyProud/index.tsx
+++ src/components/notes/subjects/physics/topics/StridavyProud/index.tsx
@@ -18,6 +18,8 @@ export default function StridavyProud() {
     <Typography><InlineMath math="\mu = U_m * sin(\alpha)"></InlineMath></Typography>
     <Typography><InlineMath math="\mu = U_m * sin(\omega t + \varphi_0)"></InlineMath></Typography>
     <Typography><InlineMath math="i = I_m * sin(\omega t + \varphi_0)"></InlineMath></Typography>
+    <Typography><InlineMath math="U_m = "></InlineMath> amplituda napětí (nejvetsi mozna hodnota)</Typography>
+    <Typography><InlineMath math="\omega t + \varphi_0 = "></InlineMath> Fáze</Typography>
     <Typography>Příklad...</Typography>
     <Typography><InlineMath math="y = y_m * sin(\omega t)"></InlineMath></Typography>
     <Typography level="h2">Jednoduché obvody střídavého proudu</Typography>
@@ -25,6 +27,16 @@ export default function StridavyProud() {
       <img src="/obvod_s_rezistorem.jpg" alt="Obvod s rezistorem" />
       <Separator />
       <img src="/obvod_s_rezistorem_2.jpg" alt="Obvod s rezistorem" />
+    </NoteAccordion>
+    <DateMark>11.5.2026</DateMark>
+    <Typography>Ohmuv zakon lze prepsat pomoci okamzitych hodnot nebo amplitud:</Typography>
+    <Typography><InlineMath math="i = \frac{u}{R}; I_m = \frac{U_m}{R}"></InlineMath></Typography>
+    <Typography>Tedy <InlineMath math="R = \frac{U_m}{I_m}"></InlineMath> - Rezistance</Typography>
+    <NoteAccordion summary="Obvod s indukcnosti">
+      <img src="/obvod_s_indukcnosti.jpg" alt="Obvod s indukcnosti" />
+      <Separator />
+      <img src="/obvod_s_indukcnosti_2.png" alt="Obvod s indukcnosti"/>
+      <Typography>Proud se opožďuje</Typography>
     </NoteAccordion>
   </div>;
 }
