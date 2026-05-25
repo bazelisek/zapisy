6c0479f1d7ec60f139f489bdb1a962bd8f4ff45c
bazelisek <windynour@gmail.com>
2026-05-25 12:24:27 +0200

    Chemie 25.5.

---
+++ src/components/notes/subjects/chemistry/topics/organika/Areny/index.tsx
@@ -0,0 +1,26 @@
+export default function Areny() {
+    return (<div>
+        <object
+        data="/materials/Areny1kdoplneni.pdf"
+        type="application/pdf"
+        width="100%"
+        height="100%"
+        style={{ minHeight: "900px" }}
+      >
+        <p>
+          <a href="/materials/Alkeny_alkyny2.pdf">Open PDF</a>
+        </p>
+      </object>
+      <object
+        data="/materials/arenynazvoslovi.pdf"
+        type="application/pdf"
+        width="100%"
+        height="100%"
+        style={{ minHeight: "900px" }}
+      >
+        <p>
+          <a href="/materials/Alkeny_alkyny2.pdf">Open PDF</a>
+        </p>
+      </object>
+    </div>)
+}
